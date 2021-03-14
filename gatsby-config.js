9015a69111fb7d35ac8220c82a0dffb5a55edc92
sergiocardoso <contato@sergiocardoso.org>
2021-03-14 10:36:44 -0300

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,26 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+    conteudo: `ola mundao`,
+  },
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-emotion`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
